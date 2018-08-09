6b65e4a3afa73b11cae9f05594da6835857ebb87
Eric Bui <ericbui2@gmail.com>
2018-08-09 15:15:26 -0500

    finished lesson 1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import { createServer } from 'http'
 
 import app from './server'
 let currentApp = app
+let port = 3001;
 
+// Only user http wrapper if you want hot module reloading or websockets
+// Otherwise, app.listen is ok!
+const server = http.createServer(app);
+server.listen(port, () => {
+	console.log(`Server is running on port: ${port}.`);
+});
+
+// app.listen(port, () => {
+// 	console.log(`Server is running on port: ${port}.`);
+// });
 
 if (module.hot) {
 	module.hot.accept(['./server'], () => {
--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ import { restRouter } from './api'
 import { connect } from './db'
 import { signin, protect } from './api/modules/auth'
 // Declare an app from express
-const app = null
+const app = express();
+
+// Test Endpoint
+app.get('/', (req, res, next) => {
+  res.status(200).json({ ok: false });
+});
 
 export default app
